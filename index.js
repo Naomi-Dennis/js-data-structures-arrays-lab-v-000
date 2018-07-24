dc8d73098c55a1c2e54e5a5ec59bf4bdfacfe2b6
Naomi Dennis <njdennis94@gmail.com>
2018-07-24 19:12:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function destructivelyAppendDriver(name){
   drivers.push(name)
 }
 function destructivelyPrependDriver(name){
-
+  drivers.unshift(name)
 }
 function destructivelyRemoveLastDriver(name){
 
